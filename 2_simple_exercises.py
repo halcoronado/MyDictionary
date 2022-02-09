be34156d41c93049c49b086b828920bb85c4e1e4
Hal Coronado <hallie_coronado1@baylor.edu>
2022-02-09 15:44:25 -0600

    feb 9

--- 2_simple_exercises.py
+++ 2_simple_exercises.py
@@ -1,7 +1,7 @@
 # 1) print out the value for the key 'history' using the dictionary below
 
 
-from random import sample
+#from random import sample
 
 
 sampleDict = { 
@@ -17,15 +17,17 @@ sampleDict = {
 }
 
 
-#for x in sampleDict['class']:
+print(sampleDict["class"]["student"]["marks"]["history"])
+
+
    
 
 # 2) Add 2 inches to the son's height.
 
 dict={"son's name": "Lucas", "son's eyes": "green", "son's height": 32, "son's weight": 25}
 
-
-
+dict["son's height"]+=2
+print(dict["son's height"])
 
 
 # 3) Given a Python dictionary, Change Brad’s salary to 8500
@@ -36,7 +38,8 @@ sampleDict = {
      'emp3': {'name': 'Brad', 'salary': 6500}
 }
 
-
+sampleDict['emp3']['salary']=8500
+print(sampleDict["emp3"]['salary'])
 
 
 
@@ -46,4 +49,5 @@ sampleDict = {
 
 dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
 
-
+dict['work']=["Apology", "Phaedo", "Republic", "Symposium"]
+print(dict)
