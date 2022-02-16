6106c2cd3ea1b20fb02ab716404c852561e6779a
halcoronado <98358717+halcoronado@users.noreply.github.com>
2022-02-16 09:47:37 -0600

    prepping

---
+++ WorldSeriesWinners.py
@@ -0,0 +1,13 @@
+###The first line in the file is the name of the team that won in 1903, and the last line is the name of the team that won in 2009. 
+##(Note the World Series was not played in 1904 or 1994. There are entries in the file indicating this.) 
+##Write a program that reads this file and creates a dictionary in which the keys are the names of the teams, 
+##and each key’s associated value is the number of times the team has won the World Series. 
+
+
+
+
+
+
+#Create another dictionary in which the keys are the years, and each key’s associated value is the name of the team that won that year.
+##The program should prompt the user for a year in the range of 1903 through 2009. 
+##It should then display the name of the team that won the World Series that year, and the number of times that team has won the World Series.
