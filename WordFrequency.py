7943508f29b283dfe0ec41d297a6f1cf680ca922
halcoronado <98358717+halcoronado@users.noreply.github.com>
2022-02-16 12:32:45 -0600

    hhmmmmm

--- WordFrequency.py
+++ WordFrequency.py
@@ -2,3 +2,19 @@
 #keys are the individual words found in the file and the values are the number of times each word appears. 
 ##For example, if the word “the” appears 128 times, the dictionary would contain an element with 'the' as the key and 128 as the value. 
 ##The program should display the frequency of each word.
+
+infile = open('AI.txt','r')
+SC = [ ',' , '.' , '”' , '’' , '"' , "'"  , '?' , '!' , '&', '@', '*' , '#' ]
+SC=str(SC)
+
+words = ''
+for x in infile:
+  words += ' ' 
+  words += infile.read()
+words = words.lower()
+for x in SC:
+  words = words.replace(x,'')
+print (words)
+  
+
+infile.close()
